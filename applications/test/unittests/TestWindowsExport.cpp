bb15d15556c2d8e41970eb52df62ae14d94f1b80
Saif Mulla <saifbinsiraj@gmail.com>
2015-12-02 20:53:25 +0000

    first commit to directory, this initial commit contains gtest library, a single unit test to check on all platforms

---
+++ applications/test/unittests/TestWindowsExport.cpp
@@ -0,0 +1,17 @@
+//TestWindowsExport.cpp
+#include <iostream>
+#include "gtest/gtest.h"
+#include "windowsExport.H"
+
+TEST(TestWindowsExport,checkValues){
+#ifdef _MSC_VER
+	ASSERT_EQ(1,FEXPORT);
+#elif defined(__linux__) || (__APPLE__)
+	ASSERT_EQ(0,FEXPORT);
+#endif
+}
+
+int main(int argc, char *argv[]){
+    ::testing::InitGoogleTest(&argc,argv);
+    return RUN_ALL_TESTS();
+}
