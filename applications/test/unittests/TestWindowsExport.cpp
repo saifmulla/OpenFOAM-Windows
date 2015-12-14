9097ed36e6d0262d19c614347baa83359928e0b0
Saif Mulla <saifbinsiraj@gmail.com>
2015-12-14 21:54:20 +0000

    code compiling on mac and also initial test compiling and running successfully until now not tested on windows

--- applications/test/unittests/TestWindowsExport.cpp
+++ applications/test/unittests/TestWindowsExport.cpp
@@ -1,15 +1,16 @@
 //TestWindowsExport.cpp
 #include <iostream>
-#include "gtest/gtest.h"
+// #include "gtest/gtest.h"
+#include "gmock/gmock.h"
 #include "windowsExport.H"
 
-TEST(TestWindowsExport,checkValues){
-#ifdef _MSC_VER
-	ASSERT_EQ(1,FEXPORT);
-#elif defined(__linux__) || (__APPLE__)
-	ASSERT_EQ(0,FEXPORT);
-#endif
-}
+ TEST(TestWindowsExport,checkValues){
+ #ifdef _MSC_VER
+ 	ASSERT_EQ(1,FEXPORT);
+ #elif defined(__linux__) || (__APPLE__)
+ 	ASSERT_EQ(0,FEXPORT);
+ #endif
+ }
 
 int main(int argc, char *argv[]){
     ::testing::InitGoogleTest(&argc,argv);
