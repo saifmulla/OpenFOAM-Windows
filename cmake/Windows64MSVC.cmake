e0474760bd3cdc5309a26054d1da20871b556efa
Saif Mulla <saifbinsiraj@gmail.com>
2016-01-16 23:13:25 +0000

    nightly-moved bool class inside bools directory
    further new classes for hashing and SHA1 although these classes utilise
    unix functionality, therefore compilation failure is inevitable on
    windows

--- cmake/Windows64MSVC.cmake
+++ cmake/Windows64MSVC.cmake
@@ -6,3 +6,14 @@ MESSAGE(STATUS "BUILD COMMAND " ${CMAKE_VS_MSBUILD_COMMAND})
 MESSAGE(STATUS "PLATFORM TOOLSET " ${CMAKE_VS_PLATFORM_TOOLSET})
 # make microsoft compiler to compile 64
 SET(CMAKE_CL_64 TRUE)
+
+# ###############################################
+# Make VC happy and group all the binary outputs,
+# also make sure GMock headers will compile in
+# all targets.
+IF( MSVC )
+	SET(CMAKE_RUNTIME_OUTPUT_DIRECTORY ${PROJECT_BINARY_DIR}/bin/)
+	SET(CMAKE_ARCHIVE_OUTPUT_DIRECTORY ${PROJECT_BINARY_DIR}/bin/)
+	SET(CMAKE_LIBRARY_OUTPUT_DIRECTORY ${PROJECT_BINARY_DIR}/lib/)
+	ADD_DEFINITIONS( -D_VARIADIC_MAX=10)
+ENDIF( MSVC )
